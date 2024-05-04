b82653da75f8cec29a226e6e24f96ac52cd21877
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-05-04 15:37:08 +0300

    add last files in project1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cards from "./Components/Cards";
+import { data } from "./data";
 
-function App() {
+export default function App() {
+  const dataShow = data.map((el) => {
+    return <Cards key={el.id} data={el.title} desc={el.desc} />;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contianer">
+      <h1>React App</h1>
+      {dataShow}
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: 1,
+    title: "Title 1",
+    desc: "desc 1",
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    desc: "desc 2",
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    desc: "desc 3",
+  },
+  {
+    id: 4,
+    title: "Title 4",
+    desc: "desc 4",
+  },
+  {
+    id: 5,
+    title: "Title 5",
+    desc: "desc 5",
+  },
+  {
+    id: 6,
+    title: "Title 6",
+    desc: "desc 6",
+  },
+];
+
+
